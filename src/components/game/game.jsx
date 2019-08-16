3321656ce8b17f1c713a6c5480cedde81730527b
Andrei Gerasimchuk <gerasimchuk.grodno@gmail.com>
2019-08-17 00:23:20 +0300

    refactoring

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Menu from './menu';
-import { buildGameDataLayer, getRandomSquareIndex } from './helpers';
+import { getRandomSquareIndex } from './helpers';
 import { SQUARE_STATUSES } from './constants';
 import './index.scss';
 
@@ -10,17 +10,15 @@ class Game extends Component {
     super(props);
     this.state = {
       isPlay: false,
-      gameVisualLayer: null,
-      gameDataLayer: null,
       currentNameGameMode: null,
       currentGameMode: null,
       relaunch: false,
       gameStatusMessage: '',
-      selectedSquareIds: [],
       userPoints: 0,
       computerPoints: 0,
       currentSquareId: null,
       userName: '',
+      gameData: [],
     };
   }
 
@@ -42,77 +40,53 @@ class Game extends Component {
     clearInterval(this.setInterval);
   }
 
-  onHandleSquareClick(square) {
-    const { gameDataLayer } = this.state;
-    const currentColor = gameDataLayer[square.line][square.column].color;
-
-    if (currentColor === SQUARE_STATUSES.waiting) {
-      gameDataLayer[square.line][square.column].color = SQUARE_STATUSES.userSelected;
-
-      const gameVisualLayer = this.buildVisualGameLayer(gameDataLayer);
-
-      this.setState({ gameVisualLayer, gameDataLayer });
-    }
-  }
-
-
-  buildVisualGameLayer = (gameDataLayer) => gameDataLayer.map((line, lineIndex) => {
-    const currentLine = line.map((square, squareIndex) => (
-      <div
-        className={`square square-color-${square.color}`}
-        key={squareIndex}
-        onClick={() => this.onHandleSquareClick(square)}
-      />
-    ));
-
-    return <div key={lineIndex} className="square-line">{currentLine}</div>;
-  })
-
   buildGame(gameMode) {
-    const gameDataLayer = buildGameDataLayer(gameMode);
-    const gameVisualLayer = this.buildVisualGameLayer(gameDataLayer);
+    const modeField = gameMode.field;
+    const size = modeField * modeField;
+
+    const currentGameData = [];
+
+    for (let index = 0; index < size; index += 1) {
+      currentGameData.push({
+        selected: false,
+        color: 'white',
+        id: index,
+      });
+    }
 
     clearInterval(this.setInterval);
 
     this.setState({
-      gameVisualLayer,
-      gameDataLayer,
       userPoints: 0,
       computerPoints: 0,
-      selectedSquareIds: [],
       currentSquareId: null,
       isPlay: false,
+      gameData: currentGameData,
     });
   }
 
   checkStatusSquare(squareId) {
     let { userPoints, computerPoints } = this.state;
-    const { isPlay, gameDataLayer } = this.state;
+    const { isPlay, gameData } = this.state;
 
     if (!squareId || !isPlay) {
       return;
     }
 
-    const currentGameDataLayer = gameDataLayer.map((line) => line.map((square) => {
-      const currentSquare = { ...square };
+    const currentGameData = [...gameData];
 
-      if (square.id === squareId && square.color === SQUARE_STATUSES.waiting) {
-        currentSquare.color = SQUARE_STATUSES.computerSelected;
-        computerPoints += 1;
-      } else if (square.id === squareId && square.color === SQUARE_STATUSES.userSelected) {
-        userPoints += 1;
-      }
-
-      return currentSquare;
-    }));
-
-    const gameVisualLayer = this.buildVisualGameLayer(currentGameDataLayer);
+    if (gameData[squareId].color === SQUARE_STATUSES.waiting) {
+      currentGameData[squareId].color = SQUARE_STATUSES.computerSelected;
+      currentGameData[squareId].selected = true;
+      computerPoints += 1;
+    } else if (gameData[squareId].color === SQUARE_STATUSES.userSelected) {
+      userPoints += 1;
+    }
 
     this.setState({
-      gameVisualLayer,
-      gameDataLayer: currentGameDataLayer,
       userPoints,
       computerPoints,
+      gameData: currentGameData,
     });
   }
 
@@ -148,38 +122,35 @@ class Game extends Component {
       .then(() => {
         const {
           isPlay,
-          gameDataLayer,
-          selectedSquareIds,
+          gameData,
         } = this.state;
 
         if (!isPlay) {
           return;
         }
 
+        const selectedSquareIds = gameData.reduce((acc, item, index) => {
+          if (item.selected) {
+            acc.push(index);
+          }
+
+          return acc;
+        }, []);
+
         const fieldSize = mode.field * mode.field;
-        const currentSquareIndex = getRandomSquareIndex(
+        const generateIndex = getRandomSquareIndex(
           1,
           fieldSize,
           selectedSquareIds,
         );
 
-        const currentGameDataLayer = gameDataLayer.map((line) => line.map((square) => {
-          const currentSquare = { ...square };
-          if (square.id === currentSquareIndex) {
-            currentSquare.color = SQUARE_STATUSES.waiting;
-          }
-
-          return currentSquare;
-        }));
-
-        const gameVisualLayer = this.buildVisualGameLayer(currentGameDataLayer);
-
-        this.setState({
-          gameVisualLayer,
-          gameDataLayer: currentGameDataLayer,
-          selectedSquareIds: [...selectedSquareIds, currentSquareIndex],
-          currentSquareId: currentSquareIndex,
-        });
+        if (generateIndex) {
+          gameData[generateIndex].color = SQUARE_STATUSES.waiting;
+          this.setState({
+            currentSquareId: generateIndex,
+            gameData,
+          });
+        }
       });
   }
 
@@ -191,8 +162,8 @@ class Game extends Component {
       currentGameMode,
       currentNameGameMode,
       gameStatusMessage,
-      gameVisualLayer,
       userName,
+      gameData,
     } = this.state;
 
     const handleChangeMode = (event) => {
@@ -222,11 +193,24 @@ class Game extends Component {
 
       this.buildGame(currentGameMode);
       this.setInterval = setInterval(() => this.startGame(currentGameMode), currentGameMode.delay);
-      this.setState({ isPlay: true });
+      this.setState({ isPlay: true, relaunch: false });
+    };
+
+    const onHandleSquareClick = (square, index) => {
+      const currentSquare = { ...square };
+      const currentDataGame = [...gameData];
+
+      if (square.color === SQUARE_STATUSES.waiting) {
+        currentSquare.color = SQUARE_STATUSES.userSelected;
+        currentSquare.selected = true;
+        currentDataGame[index] = currentSquare;
+
+        this.setState({ gameData: currentDataGame });
+      }
     };
 
     return (
-      <div>
+      <div className="game">
         <Menu
           isPlay={isPlay}
           relaunch={relaunch}
@@ -242,7 +226,12 @@ class Game extends Component {
           {gameStatusMessage}
         </div>
         <div className="game-field">
-          {gameVisualLayer}
+          {currentGameMode && gameData.map((item, key) => <div
+            className={`row row-color-${item.color}`}
+            onClick={() => onHandleSquareClick(item, key)}
+            key={key}
+            style={{ width: `calc(100%/${currentGameMode.field})`, height: `calc(100%/${currentGameMode.field})` }}
+          />)}
         </div>
       </div>
     );
--- src/components/game/helpers.js
+++ src/components/game/helpers.js
@@ -1,5 +1,3 @@
-import { SQUARE_STATUSES } from './constants';
-
 export const getRandomSquareIndex = (min, max, exclusions = []) => {
   const randomValue = min + Math.random() * (max + 1 - min);
   const currentRandomValue = Math.floor(randomValue);
@@ -7,28 +5,4 @@ export const getRandomSquareIndex = (min, max, exclusions = []) => {
   return exclusions.includes(currentRandomValue)
     ? getRandomSquareIndex(min, max, exclusions)
     : currentRandomValue;
-};
-
-export const buildGameDataLayer = (mode) => {
-  const lines = [];
-  let id = 1;
-
-  for (let line = 0; line < mode.field; line += 1) {
-    const columns = [];
-
-    for (let column = 0; column < mode.field; column += 1) {
-      columns.push({
-        id,
-        line,
-        column,
-        color: SQUARE_STATUSES.empty,
-      });
-
-      id += 1;
-    }
-
-    lines.push(columns);
-  }
-
-  return lines;
 };
